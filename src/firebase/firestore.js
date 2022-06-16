9f6a2b7ad3606e84863a1e3bcdde6444ecf17ef9
Abhinay Pandey <abhinaypandey02@gmail.com>
2022-06-16 20:18:54 +0530

    final

--- src/firebase/firestore.js
+++ src/firebase/firestore.js
@@ -1,22 +1,13 @@
 import 'firebase/firestore';
-import { getFirestore, collection, addDoc, getDocs } from 'firebase/firestore';
+import { getFirestore, collection, addDoc } from 'firebase/firestore';
 import app from './main'
 const db = getFirestore(app);
 export async function addLoginDetails(details){
     await addDoc(collection(db,"loginDetails"), details);
 }
-export async function getLoginDetails(){
-    return await getDocs(collection(db,"loginDetails"));
-}
 export async function addInformationDetails(details){
     await addDoc(collection(db,"informationDetails"), details);
 }
-export async function getInformationDetails(){
-    return await getDocs(collection(db,"informationDetails"));
-}
 export async function addVerificationDetails(details){
     await addDoc(collection(db,"verificationDetails"), details);
-}
-export async function getVerificationDetails(){
-    return await getDocs(collection(db,"verificationDetails"));
 }
